328fe739785acb4c27b6293411e547292b3e7302
SRattacasa <38533+SRattacasa@users.noreply.github.com>
2020-05-03 20:16:26 -0400

    Finally got the API calls mapped and rendered to character component

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,31 @@
 // Write your Character component here
+import React from 'react';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+  } from 'reactstrap';
+
+const Character = (props) => {
+  
+
+    return (
+        <Card>
+        <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+        <CardBody>
+          <CardTitle>{props.name}</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content. <a href={props.homeworld}>Homeworld API URL </a> </CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card>
+      
+      
+          
+        
+        
+      
+    );
+  }
+  
+  export default Character;
+  
